df95d8f58026bff0827f2ab24027e67ae926eb86
stwz13 <s.perederina@inbox.ru>
2026-02-18 08:31:20 +0600

    inheritance

--- bloom_counter_filter.py
+++ bloom_counter_filter.py
@@ -1,35 +1,15 @@
 from bloom_filter import BloomFilter
-import numpy as np
 
-class BloomCounterFilter:
-    def __init__(self, k: int, m: int, eps: float = None, n: int = None):
-        if eps is not None and n is not None:
-            self.__m = np.ceil(-n * np.log(eps) / (np.log(2) ** 2))
-            self.__k = np.ceil(self.__m * np.log(2) / n)
-        elif k is not None and m is not None:
-            self.__k = k
-            self.__m = m
-        else:
-            raise TypeError("Check the arguments")
-        self.__counters = [0] * self.__m
-
-    def add_element(self, element: str):
-        for number_of_hash_function in range(self.__k):
-            count_number = BloomFilter.get_hash(element, number_of_hash_function) % self.__m
-            self.__counters[count_number] += 1
-
-    def check_element_in_filter(self, element: str):
-        for number_of_hush_function in range(self.__k):
-            count_number = BloomFilter.get_hash(element, number_of_hush_function) % self.__m
-            if self.__counters[count_number] == 0:
-                return False
-        return True
+class BloomCounterFilter(BloomFilter):
+    def __init__(self, k : int = None, m: int = None, eps: float = None, n: int = None):
+        super().__init__(k, m, eps, n)
+        self._counters = [0] * self._m
 
     def remove_element(self, element: str):
         if not self.check_element_in_filter(element):
             raise ValueError("Element not in the filter")
-        for number_of_hush_function in range(self.__k):
-            count_number = BloomFilter.get_hash(element, number_of_hush_function) % self.__m
-            self.__counters[count_number] -= 1
+        for number_of_hush_function in range(self._k):
+            count_number = BloomFilter._get_hash(element, number_of_hush_function) % self._m
+            self._counters[count_number] -= 1
 
 
--- bloom_filter.py
+++ bloom_filter.py
@@ -7,34 +7,34 @@ import bitarray
 class BloomFilter:
     def __init__(self, k: int = None, m: int = None, eps: float = None , n: int = None):
         if eps is not None and n is not None:
-            self.__m = int(np.ceil(-n * np.log(eps) / (np.log(2) ** 2)))
-            self.__k = int(np.ceil(self.__m * np.log(2) / n))
+            self._m = int(np.ceil(-n * np.log(eps) / (np.log(2) ** 2)))
+            self._k = int(np.ceil(self._m * np.log(2) / n))
         elif k is not None and m is not None:
-            self.__m = m
-            self.__k = k
+            self._m = m
+            self._k = k
         else:
             raise TypeError("Need a pare of parameters k and m or eps and n")
-        self.__bite_array = bitarray.bitarray(self.__m)
+        self._counters = bitarray.bitarray(self._m)
 
 
-    def add_to_filter(self, element: str):
-        for number_of_hash_func in range(self.__k):
-            array_index = self.get_hash(element, number_of_hash_func) % self.__m
-            self.__bite_array[array_index] = 1
+    def add_element(self, element: str):
+        for number_of_hash_func in range(self._k):
+            array_index = self._get_hash(element, number_of_hash_func) % self._m
+            self._counters[array_index] += 1
 
 
     @staticmethod
-    def get_hash(element: str, number_of_function: int):
+    def _get_hash(element: str, number_of_function: int):
         return mmh3.hash128(str(number_of_function) + element) % (2**30)
 
 
     def check_element_in_filter(self, element: str):
-        for number_of_hash_func in range(self.__k):
-            number = self.get_hash(element, number_of_hash_func) % self.__m
-            if self.__bite_array[number] == 0:
+        for number_of_hash_func in range(self._k):
+            number = self._get_hash(element, number_of_hash_func) % self._m
+            if self._counters[number] == 0:
                 return False
         return True
 
 
 bf = BloomFilter(eps=0.01, n=15)
-bf.add_to_filter("F")
+bf.add_element("F")
